c1048a890305318de56e765752fc88fa82277e65
Jaco Kok <kokjaco2@gmail.com>
2024-09-06 23:54:51 +0200

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              ansible
+              sshpass
+            ];
+            shellHook = ''
+              echo "$(ansible --version)"
+            '';
+          };
+        }
+      );
+}
